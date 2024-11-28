032988a91e7ebef6074c54658dcf6f8d1fd22284
Kovokar <65097883+Kovokar@users.noreply.github.com>
2024-11-27 23:27:08 -0300

    Atvd3

---
+++ atvd3/Q6.ts
@@ -0,0 +1,12 @@
+const nome: string = 'Ely';
+const paymentTime: number = 120.56;
+const language: string = 'TypeScript';
+
+// Correção: Adicionando as crases ` (backticks) para definir o template literal
+const message: string = `${nome}
+My payment time is ${paymentTime} and
+my preferred language is ${language}`;
+
+
+
+console.log(message);
