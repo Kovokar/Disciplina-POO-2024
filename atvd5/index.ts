7a8ecb2000231ab9dc715a509f91a132979e511f
Kovokar <65097883+Kovokar@users.noreply.github.com>
2024-12-08 21:02:04 -0300

    Update index.ts

--- atvd5/index.ts
+++ atvd5/index.ts
@@ -1,35 +1,48 @@
-import { Cliente } from './Cliente/Cliente';
-import { Banco } from './Banco/Banco';
-import { Conta } from './Conta/Conta';
+import { Cliente } from './Cliente/Cliente'
+import { Banco } from './Banco/Banco'
+import { Conta } from './Conta/Conta'
 
 // Inicializando o banco
-let banco: Banco = new Banco();
+let banco: Banco = new Banco()
 
-// Criando clientes
-const cliente1 = new Cliente(1, 'João', '1');
-const cliente2 = new Cliente(2, 'Maria', '2');
-const cliente3 = new Cliente(3, 'Carlos', '3');
-
-// Inserindo clientes no banco
-banco.inserirCliente(cliente1);
-banco.inserirCliente(cliente2);
-banco.inserirCliente(cliente3);
-banco.inserirCliente(cliente1);
-// console.log(banco.clientes)
+// // Criando clientes
+const cliente1 = new Cliente(1, 'João', '1')
+const cliente2 = new Cliente(2, 'Maria', '2')
+const cliente3 = new Cliente(3, 'Carlos', '3')
+const cliTevis = new Cliente(4, 'Tevis', '4')
 
 
 
-banco.inserirConta(new Conta('111-1', 100, 1));
-banco.inserirConta(new Conta('222-2', 200, 2));
-banco.inserirConta(new Conta('333-3', 300, 3));
-banco.inserirConta(new Conta('444-4', 400, 4));
+banco.inserirCliente(cliente1)
+banco.inserirCliente(cliente2)
+banco.inserirCliente(cliente3)
+banco.inserirCliente(cliTevis)
+console.log('')
+banco.inserirConta(new Conta('111-1', 100, 1))
+banco.inserirConta(new Conta('222-2', 200, 2))
+banco.inserirConta(new Conta('333-3', 300, 3))
+banco.inserirConta(new Conta('444-4', 400, 4))
 
-// Consultando e exibindo os clientes inseridos
-// console.log(banco.consultaPorCpf('1')); // Deve retornar João
-// console.log(banco.consultaPorCpf('2')); // Deve retornar Maria
-// console.log(banco.consultaPorCpf('3')); // Deve retornar Carlos
 
-// banco.associarContaCliente('111-1', '2')
-// banco.associarContaCliente('222-2', '2')
+//TESTANDO REGRAS DE NEGOCIO
+console.log('')
+banco.associarContaCliente('111-1','4')
+banco.associarContaCliente('222-2','4')
+banco.associarContaCliente('222-2','1') //deve gerar erro
+console.log('')
 
-console.log(banco.totalizadorSaldoCliente('2'))
+
+const cliIdRepetido = new Cliente(4, 'Felipe','5')
+const cliCPFRepetido = new Cliente(5, 'teste', '1')
+
+banco.inserirCliente(cliIdRepetido) // deve gerar erro
+banco.inserirCliente(cliCPFRepetido) // deve gerar erro
+console.log('')
+
+banco.inserirConta(new Conta('555-5', 500, 4)) // deve gerar erro
+banco.inserirConta(new Conta('444-4', 500, 5)) // deve gerar erro
+
+
+console.log(banco.listarContasDeUmCliente('4'))
+
+console.log(banco.totalizadorSaldoCliente('4'))
