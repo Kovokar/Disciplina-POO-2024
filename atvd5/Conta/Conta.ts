bc70fb8d8e0e10e97d034b10b4d3a7cc217ce1e8
Kovokar <65097883+Kovokar@users.noreply.github.com>
2024-12-08 21:01:11 -0300

    Update Conta.ts

--- atvd5/Conta/Conta.ts
+++ atvd5/Conta/Conta.ts
@@ -5,11 +5,13 @@ export class Conta {
     saldo: number
     id_conta: number
     cliente: Cliente
+    st_assosciada: boolean //esta associada
 
     constructor(numero: string, saldo: number, id_conta: number) {
         this.numero = numero
         this.saldo = saldo
         this.id_conta = id_conta
+        this.st_assosciada = false
         
     }
 
