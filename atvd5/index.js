a136591da79b957e9781fa07d3d36ffbf4ec43cf
Kovokar <65097883+Kovokar@users.noreply.github.com>
2024-12-08 21:01:48 -0300

    Update index.js

--- atvd5/index.js
+++ atvd5/index.js
@@ -5,24 +5,32 @@ var Banco_1 = require("./Banco/Banco");
 var Conta_1 = require("./Conta/Conta");
 // Inicializando o banco
 var banco = new Banco_1.Banco();
-// Criando clientes
+// // Criando clientes
 var cliente1 = new Cliente_1.Cliente(1, 'João', '1');
 var cliente2 = new Cliente_1.Cliente(2, 'Maria', '2');
 var cliente3 = new Cliente_1.Cliente(3, 'Carlos', '3');
-// Inserindo clientes no banco
+var cliTevis = new Cliente_1.Cliente(4, 'Tevis', '4');
 banco.inserirCliente(cliente1);
 banco.inserirCliente(cliente2);
 banco.inserirCliente(cliente3);
-banco.inserirCliente(cliente1);
-// console.log(banco.clientes)
+banco.inserirCliente(cliTevis);
+console.log('');
 banco.inserirConta(new Conta_1.Conta('111-1', 100, 1));
 banco.inserirConta(new Conta_1.Conta('222-2', 200, 2));
 banco.inserirConta(new Conta_1.Conta('333-3', 300, 3));
 banco.inserirConta(new Conta_1.Conta('444-4', 400, 4));
-// Consultando e exibindo os clientes inseridos
-// console.log(banco.consultaPorCpf('1')); // Deve retornar João
-// console.log(banco.consultaPorCpf('2')); // Deve retornar Maria
-// console.log(banco.consultaPorCpf('3')); // Deve retornar Carlos
-// banco.associarContaCliente('111-1', '2')
-// banco.associarContaCliente('222-2', '2')
-console.log(banco.totalizadorSaldoCliente('2'));
+//TESTANDO REGRAS DE NEGOCIO
+console.log('');
+banco.associarContaCliente('111-1', '4');
+banco.associarContaCliente('222-2', '4');
+banco.associarContaCliente('222-2', '1'); //deve gerar erro
+console.log('');
+var cliIdRepetido = new Cliente_1.Cliente(4, 'Felipe', '5');
+var cliCPFRepetido = new Cliente_1.Cliente(5, 'teste', '1');
+banco.inserirCliente(cliIdRepetido); // deve gerar erro
+banco.inserirCliente(cliCPFRepetido); // deve gerar erro
+console.log('');
+banco.inserirConta(new Conta_1.Conta('555-5', 500, 4)); // deve gerar erro
+banco.inserirConta(new Conta_1.Conta('444-4', 500, 5)); // deve gerar erro
+console.log(banco.listarContasDeUmCliente('4'));
+console.log(banco.totalizadorSaldoCliente('4'));
